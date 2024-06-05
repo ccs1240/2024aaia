8ebf519f37db821b8c85940ae7ffd399ea3c70b9
ccs1240 <144758116+ccs1240@users.noreply.github.com>
2024-06-05 11:41:24 +0800

    Delete week16-1.py

--- week16-1.py
+++
@@ -1,9 +0,0 @@
-#16
-from collections import Counter #import collections
-words=["bella","lebel","roller"]
-for i in range(3):
-  counter =Counter(words[i]) #counter=collections.Counter(word[i])
-  print(words[i])
-  print(counter)
-ans =Counter(words[0])&Counter(words[1])&Counter(words[2]) #all of Counter before have a collections and a punctuation
-print(ans)
